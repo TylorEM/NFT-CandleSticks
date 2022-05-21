26e849fe99c677e1bc0a68e717fb969784dcaa09
TylorEM <TylorEM@users.noreply.github.com>
2022-05-21 09:51:34 -0400

    test commit w/ OpenSea API

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import FetchWrapper from './helpers';
 
-//const API = new FetchWrapper('https://api.opensea.io/api/v1/collection');
+const API = new FetchWrapper('https://api.opensea.io/api/v1/collection');
 
 API.get('/kaiju-kingz/stats')
   .then((data) => console.log(data.stats.floor_price))
